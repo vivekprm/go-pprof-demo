7983ce4b16d38fd72e6675e452a77170632df73c
Vivek Mishra <v.mishra@f5.com>
2023-07-24 12:00:10 +0530

    optimized by removing regexp replacement

--- stats/reporter_test.go
+++ stats/reporter_test.go
@@ -42,3 +42,15 @@ func TestAddTagsToName(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkAddTagsToName(b *testing.B) {
+	tags := map[string]string{
+		"host":     "myhost",
+		"endpoint": "hello",
+		"os":       "OS X",
+		"browser":  "Chrome",
+	}
+	for i := 0; i < b.N; i++ {
+		addTagsToName("recv.calls", tags)
+	}
+}
